62573543dbd6e82d76dcc64d309486ab2d88a823
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-14 23:36:37 +0000

    create RightSideBar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
   const loggedIn = { 
-    firstName: "Tiago"
+    firstName: "Tiago",
+    lastName: "Sousa",
+    email: "teste@teste.com"
   }
   return (
     <section className='home'>
@@ -23,7 +26,15 @@ const Home = () => {
             totalCurrentBalance={1250.27}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{}, {}]}
+      />
     </section>
   )
 }
