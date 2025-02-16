eba64049638d6a36197f9eca7663ed409db81e84
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-16 23:40:06 +0000

    dotenv configurations

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,19 +15,14 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
-function ClientOnlyComponent() {
-   const [clientData, setClientData] = useState("");
- 
-   useEffect(() => {
-     setClientData("Client Rendered Data");
-   }, []);
- 
-   return <div>{clientData}</div>;
- }
+
 
 
 const AuthForm = ({type} : {type:string}) => {
+   const router = useRouter()
    const [user, setUser] = useState(null)
    const [isLoading,setIsLoading] = useState(false)
 
@@ -43,12 +38,33 @@ const AuthForm = ({type} : {type:string}) => {
  })
 
  // 2. Define a submit handler.
- function onSubmit(values: z.infer<typeof formSchema>) {
-   // Do something with the form values.
-   // ✅ This will be type-safe and validated.
+ const onSubmit = async (data: z.infer<typeof formSchema>) => {
+   
    setIsLoading(true)
-   console.log(values)
-   setIsLoading(false)
+
+   try {
+      // sign up with appwrite & create plaid token
+
+      if(type === 'sign-up') {
+         const newUser = await signUp(data)
+         setUser(newUser)
+      } 
+
+      if(type === 'sign-in') {
+         const response = await signIn({
+            email: data.email,
+            passoword: data.password
+         })
+
+         if(response) router.push('/')
+      }
+
+
+   } catch(error) {
+      console.log("error: ",error)
+   } finally {
+      setIsLoading(false)
+   }
  }
 
 
@@ -109,6 +125,12 @@ const AuthForm = ({type} : {type:string}) => {
                      name='address1'
                      label='Address'
                      placeholder='Enter your Address'
+                   />
+                    <CustomInput 
+                     control={form.control}
+                     name='city'
+                     label='City'
+                     placeholder='Enter your City'
                    />
 
                  <div className='flex gap-4'>
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,16 @@
+'use server'
+
+export const signIn = async () => {
+   try {
+      //mutation  / database / make fetch
+   } catch(error) {
+      console.log('Error:' , error)
+   }
+}
+export const signUp = async (userData : SignUpParams) => {
+   try {
+      //create a user account
+   } catch(error) {
+      console.log('Error:' , error)
+   }
+}
