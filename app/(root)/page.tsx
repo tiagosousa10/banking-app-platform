2bfe3105de161a102349bf95e0b6603d09a9e26a
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-17 00:58:28 +0000

    logout functionalities and fix some issues about navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-const Home = () => {
-  const loggedIn = { 
-    firstName: "Tiago",
-    lastName: "Sousa",
-    email: "teste@teste.com"
-  }
+const Home = async () => {
+  const loggedIn = await getLoggedInUser() //ex.{firstName: "Tiago", lastName: "Sousa"}
+  console.log("🚀 ~ Home ~ loggedIn:", loggedIn)
   return (
     <section className='home'>
       <div className='home-content'>
@@ -16,7 +14,7 @@ const Home = () => {
           <HeaderBox 
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently"
           />
 
