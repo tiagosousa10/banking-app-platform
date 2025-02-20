cdb99904dd1801acb508b00cef6b737dad2f2d4c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-20 11:44:05 +0000

    transactions page finished

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,6 +1,8 @@
 import HeaderBox from '@/components/HeaderBox'
+import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { formatAmount } from '@/lib/utils';
 import React from 'react'
 
 const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) => {
@@ -33,8 +35,25 @@ const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) 
           <div className='flex flex-col gap-2'>
             <h2 className='text-18 font-bold text-white'>{account?.data.name}</h2>
             <p className='text-14 text-blue-25'>{account?.data.officialName}</p>
+            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+              ●●●● ●●●● ●●●● {account?.data.mask}
+            </p>
+          </div>
+
+          <div className='transactions-account-balance'>
+            <p className='text-14'>
+             Current Balance 
+            </p>
+            <p className='text-24 text-center font-bold'>{formatAmount(account?.data.currentBalance)} </p>
           </div>
         </div>
+
+        <section className='flex w-full flex-col gap-6'>
+          <TransactionsTable 
+            transactions={account?.transactions}
+          
+          />
+        </section>
       </div>
     </div>
   )
