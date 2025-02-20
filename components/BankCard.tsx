d6164cfc1dddc15b9f86a0d0c50a3bbcaf57a916
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-20 11:55:38 +0000

    sharaeble id to copy

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,6 +2,7 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
 
@@ -58,7 +59,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
           className="absolute top-0 left-0"
         />
       </Link>
-
+      {showBalance && <Copy title={account?.sharableId} />}
     </div>
   )
 }
