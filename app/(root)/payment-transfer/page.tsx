1476c7b364dd7b1a906893a07d6fb61b102d8c39
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-20 12:25:36 +0000

    paymenttransfer form added

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,8 +1,19 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Transfer = () => {
   return (
-    <div>Transfer</div>
+    <section className='payment-transfer'>
+      <HeaderBox 
+        title='Payment Transfer'
+        subtext='Please provide the necessary details to initiate a payment transfer.'
+      />
+
+      <section className='size-full pt-5'>
+        
+      </section>
+
+    </section>
   )
 }
 
