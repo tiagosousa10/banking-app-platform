56de97a5bda9b1ffd72ad510e05a265aa769c543
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-20 11:37:25 +0000

    starting transaction history page

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,8 +1,42 @@
+import HeaderBox from '@/components/HeaderBox'
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const TransactionHistory = () => {
+const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) => {
+    const currentPage = Number(page as string) || 1;
+    const loggedIn = await getLoggedInUser(); // get logged in user
+
+    const accounts = await getAccounts({ 
+      userId: loggedIn.$id 
+    })
+    if(!accounts) return;
+    
+    const accountsData = accounts?.data;
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+    const account = await getAccount({ appwriteItemId })
+
+
+
   return (
-    <div>TransactionHistory</div>
+    <div className='transactions'>
+      <div className='transactions-header'>
+        <HeaderBox 
+          title='Transaction History'
+          subtext='See your bank details and transactions.'
+        />
+      </div>
+
+      <div className='space-y-6'>
+        <div className='transacions-account'>
+          <div className='flex flex-col gap-2'>
+            <h2 className='text-18 font-bold text-white'>{account?.data.name}</h2>
+            <p className='text-14 text-blue-25'>{account?.data.officialName}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -3,6 +3,9 @@ import { Button } from './ui/button'
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
 import { useRouter } from 'next/navigation';
 import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions';
+import Image from 'next/image';
+
+
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter();
   const [token, setToken] = useState('');
@@ -38,12 +41,14 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
           Connect bank
         </Button>
       ): variant === 'ghost' ? (
-        <Button>
-          Connect bank
+        <Button onClick={() => open()}  className='plaidlink-ghost' variant="ghost">
+          <Image src="/icons/connect-bank.svg" width={24} height={24} />
+          <p className='hidden text-[16px] font-semibold text-black-2 xl:block'>Connect bank</p>      
         </Button>
       ): (
-        <Button>
-          Connect bank
+        <Button onClick={() => open()}  className='plaidlink-default'>
+          <Image src="/icons/connect-bank.svg" width={24} height={24} />
+          <p className='text-[16px] font-semibold text-black-2'>Connect bank</p>
         </Button>
       )}
     </>
